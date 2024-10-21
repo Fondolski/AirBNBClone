35f906d71463a605a4f6a11af434a09df50b413b
unknown <fondolski@gmail.com>
2024-10-22 02:08:32 +0300

    uppercase edit

--- src/app/pages/listing/page.js
+++ src/app/pages/listing/page.js
@@ -4,12 +4,10 @@
 import {useEffect} from 'react'
 import { useSearchParams, usePathname, useParams } from 'next/navigation';
 
-export default function listingPage() {
+export default function ListingPage() {
 
 
-    useEffect(()=> {
-        //console.log( params, '1')
-    },[])
+    
 
     return(
         <main className="flex min-h-screen flex-col items-center bg-white">
